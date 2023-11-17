7eaacbce1baf504877c37e4024c99bd73a8238cb
Matheus Dias <dias.matheus.eletrica@gmail.com>
2023-11-17 16:12:22 -0300

    Versão 1

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Atmega328P_LED_BLINK
+
+Para todas as versões, é importante filtrar o contact bounce com um capacitor de 100 nF (código 104 p/ cerâmico)
+Versão 1: Sem interrupção externa.
+Versão 2: Com interrupção externa
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/led_blink_v1.c
@@ -0,0 +1,93 @@
+/*
+Fazer um programa que a cada toque no botão (PB4) mude o  estados de dois leds
+colocados na porta PC0 e PC1, na seguinte sequência
+▪ 1 - Led PC0 aceso
+▪ 2 - Led PC1 aceso
+▪ 3 – Led PC0 piscando em 5 Hz
+▪ 4 – Led PC0 apagado
+▪ 5 – Led PC1 piscando em 2 Hz
+▪ 6 – Leds apagados 
+*/
+
+#include<avr/io.h>
+
+#define F_CPU 1600000
+#include <avr/io.h>
+#include <util/delay.h>
+
+enum{
+	PC0_ON = 0,
+	PC1_ON,
+	PC0_BLINK_5HZ,
+	PC0_OFF,
+	PC1_BLINK_2HZ,
+	ALL_OFF};
+
+int state = ALL_OFF;
+
+int main(void){
+	//CONF SAÍDAS.
+	DDRC = (DDRC | (1<<0)) | (1<<1);
+
+	//CONF ENTRADAS.
+	DDRB &= ~(1<<4);
+	PORTB |= (1<<4);
+	
+	while(1){
+		switch(state){
+			case PC0_ON:
+				PORTC |= (1<<0);
+				if(!(PINB & (1<<4))){
+                    state =	PC1_ON;
+					while(!(PINB & (1<<4)));	
+				}
+			break;			
+			case PC1_ON:
+				PORTC |= (1<<1);
+				if(!(PINB & (1<<4))){
+                    state =	PC0_BLINK_5HZ;
+					while(!(PINB & (1<<4)));	
+				}
+			break;
+			case PC0_BLINK_5HZ:
+				while(1){
+					PORTC ^= (1<<0);
+					_delay_ms(200);
+					if(!(PINB & (1<<4))){
+						while(!(PINB & (1<<4)));
+						state =  PC0_OFF;
+						break;
+					}
+				}
+			break;
+			case PC0_OFF:
+				PORTC &= ~(1<<0);
+				if(!(PINB & (1<<4))){
+					while(!(PINB & (1<<4)));
+					state =	PC1_BLINK_2HZ;
+				}
+			break;
+			case PC1_BLINK_2HZ:
+				while(1){
+					PORTC ^= (1<<1);
+					_delay_ms(200);
+						if(!(PINB & (1<<4))){
+							while(!(PINB & (1<<4)));
+							state =  ALL_OFF;
+							break;
+						}
+				}
+				break;
+			case ALL_OFF:
+				PORTC = ((PORTC & ~(1<<0)) & ~(1<<1));
+				if(!(PINB &(1<<4))){
+                    state = PC0_ON;	
+					while(!(PINB &(1<<4)));	
+				}
+			break;
+				
+			
+			
+		}//FIM SWITCH.
+	}//FIM WHILE(1).
+}//FIM MAIN.
